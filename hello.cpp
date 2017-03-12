2ba1e7397bbbac5857ff90dc86f1b4f8236cd806
Bobi Permana Sandi <bobipermana78@gmail.com>
2017-03-12 20:55:56 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	cout << "Hello World"<<endl;
+	cout << "I'am Bobi Permana Sandi"<<endl;
+	cout << "This is my first program in C++"<<endl;
+	cout << "Thank's for attention";
+	return 0;
+}
