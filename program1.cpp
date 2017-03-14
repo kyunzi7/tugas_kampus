4980a5bc5d1bc07bd583a3944f98cd01576034c4
Bobi Permana Sandi <bobipermana78@gmail.com>
2017-03-14 22:59:13 +0700

    Add files via upload

---
+++ program1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	int a, b, c;
+	float f;
+	
+	a = 10;
+	b = 20;
+	c = a + b;
+	
+	cout << "10 + 20 = " << c << endl;
+	f = 70.0/3.0;
+	
+	cout << "70 / 3 = " << f << endl;
+	cout << "Terima Kasih" << endl;
+	return 0;
+}
